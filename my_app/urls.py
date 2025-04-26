7bffa09cdde65be7097918208a0e39f01f12854c
Umar Siddiqui <umarsid2002@gmail.com>
2025-04-25 23:05:26 -0500

    youtube video title fetch functionality added

--- my_app/urls.py
+++ my_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login', views.user_login, name='index'),
     path('signup', views.user_signup, name='signup'),
     path('logout', views.user_logout, name='logout'),
-    # path('generate-blog', views.generate_blog),
+    path('generate-blog', views.generate_blog),
     path('blog-list', views.blog_list, name='blog-list'),
     path('youtube-downloader', views.youtube_downloader, name='blog-list'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -4,6 +4,14 @@ from django.contrib.auth import authenticate ,login, logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+import json
+from django.conf import settings
+import os
+import assemblyai as aai
+from google import genai
+import yt_dlp
+from .models import BlogPost
+from django.contrib import messages
 
 # Create your views here.
 @login_required
@@ -48,8 +56,31 @@ def user_logout(request):
     logout(request)
     return redirect('/')
 
+@csrf_exempt
+def generate_blog(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            yt_link = data['link']
+        except (KeyError, json.JSONDecodeError):
+            return JsonResponse({'error': 'invalid data sent'}, status= 400)
+
+        # Get YT Title 
+        title = yt_title(yt_link)
+        return JsonResponse({'content': title})
+
 def youtube_downloader(request, output_folder="C:/Users/umars/Downloads"):
     return render(request, 'video_downloader.html')
 
 def blog_list(request):
     return render(request, 'all-blogs.html')
+
+def yt_title(url):
+    ydl_opts = {
+        'quiet': True,  # Suppress output
+        'no_warnings': True,  
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        return info.get('title', 'Title not found')
